5d59c417c88f23a765906d5c4055143f49d40657
Simon Tirant <simon.tirant@gmail.com>
2021-12-02 20:28:08 +0100

    implémentation du router + composant Survey

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
 import Home from './pages/Home';
+import Survey from './pages/Survey';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/survey' element={<Survey />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
