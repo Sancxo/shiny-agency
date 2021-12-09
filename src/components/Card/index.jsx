870dcd65b1b8e44483451e181a362c013c2fc948
Simon Tirant <simon.tirant@gmail.com>
2021-12-09 21:16:48 +0100

    styled components added

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,25 @@
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import DefaultPicture from '../../assets/profile.png';
+
+// Styled Components
+const CardLabel = styled.span`
+    color: #5843e4;
+    font-size: 22px;
+    font-weight: bold;
+`
+const CardImage = styled.img`
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+`
 
 function Card({label, title, picture}) {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-            <span>{label}</span>
-            <img src={picture} alt="freelance" height={80} width={80} />
-            <span>{title}</span>
+            <CardLabel>{label}</CardLabel>
+            <CardImage src={picture} alt="freelance" height={80} width={80} />
+            <CardLabel>{title}</CardLabel>
         </div>
     )
 }
