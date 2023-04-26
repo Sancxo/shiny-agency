b6f0e644368f7a58d92ccd5cc8d2c5d338f6c74b
Simon <simon@7lieues.io>
2023-04-26 23:10:18 +0200

    feat: fetches question from Backend and adds a Loader in CSS

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,20 +1,49 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import { Loader } from "../../utils/style/Atoms";
 
 function Survey() {
-    const {questionNumber} = useParams();
+    const { questionNumber } = useParams();
     const questionNumberInt = parseInt(questionNumber);
     const previousQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
     const nextQuestionNumber = questionNumberInt + 1;
 
-    return(
+    const [questions, setQuestions] = useState({});
+    const [isDataLoading, setIsDataLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchSurvey() {
+            setIsDataLoading(true);
+            try {
+                const resp = await fetch('http://localhost:8000/survey');
+                const { surveyData } = await resp.json();
+                setQuestions(surveyData);
+            } catch (err) {
+                setError(err);
+            }
+            finally {
+                setIsDataLoading(false)
+            }
+        }
+        fetchSurvey();
+    }, []);
+
+    if (error) return <p>Oups ... Il y a eu un problème !</p>
+    return (
         <div>
             <h1>Questionnaire</h1>
             <h2>Question n°{questionNumber}</h2>
 
+            {isDataLoading ?
+                <Loader /> :
+                <p>{questions[questionNumber]}</p>
+            }
+
             <Link to={`/survey/${previousQuestionNumber}`}>Précédent</Link>
-            { questionNumberInt === 10 ? <Link to='/results'>Résultats</Link> : <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link> }
-            
+            {questions[nextQuestionNumber] ? <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link> : <Link to='/results'>Résultats</Link>}
+
         </div>
     )
 }
