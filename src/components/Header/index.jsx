3362520d40b4e27ef78f68ba37c9dfd9a82e102d
Simon <simon@7lieues.io>
2023-04-26 23:42:09 +0200

    style: styling Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import colors from '../../utils/style/colors';
+import DarkLogo from '../../assets/dark-logo.png'
 
 // Styled component
 const StyledLink = styled(Link)`
@@ -8,21 +9,34 @@ const StyledLink = styled(Link)`
     color: #8186a0;
     text-decoration: none;
     font-size: 18px;
-    ${(props) => 
+    ${(props) =>
         props.$isFullLink && `
             color: white;
             border-radius: 30px;
             background: ${colors.primary};
         `}
 `
+const HomeLogo = styled.img`
+  height: 70px;
+`
+
+const NavContainer = styled.nav`
+padding: 30px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+`
 
 function Header() {
     return (
-        <nav>
-            <StyledLink to="/">Accueil</StyledLink>
-            <StyledLink to="/survey/1" $isFullLink>Faire le test</StyledLink>
-            <StyledLink to='/freelances'>Freelances</StyledLink>
-        </nav>
+        <NavContainer>
+            <Link to="/"><HomeLogo src={DarkLogo} /></Link>
+            <div>
+                <StyledLink to="/">Accueil</StyledLink>
+                <StyledLink to="/survey/1" $isFullLink>Faire le test</StyledLink>
+                <StyledLink to='/freelances'>Freelances</StyledLink>
+            </div>
+        </NavContainer>
     )
 }
 
