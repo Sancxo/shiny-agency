a9129854fc3292b65e3c4a1fce9b51bdd7f1e8f9
Simon <simon@7lieues.io>
2023-05-01 16:14:53 +0200

    style: adds style to Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,12 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import colors from '../../utils/style/colors';
-import DarkLogo from '../../assets/dark-logo.png'
+import DarkLogo from '../../assets/dark-logo.png';
+import LightLogo from '../../assets/light-logo.png';
+import { StyledLink } from '../../utils/style/Atoms';
+import { ThemeContext } from '../../utils/context';
 
 // Styled component
-const StyledLink = styled(Link)`
-    padding: 15px;
-    color: #8186a0;
-    text-decoration: none;
-    font-size: 18px;
-    ${(props) =>
-        props.$isFullLink && `
-            color: white;
-            border-radius: 30px;
-            background: ${colors.primary};
-        `}
-`
 const HomeLogo = styled.img`
   height: 70px;
 `
@@ -28,9 +19,11 @@ align-items: center;
 `
 
 function Header() {
+    const { theme } = useContext(ThemeContext);
+
     return (
         <NavContainer>
-            <Link to="/"><HomeLogo src={DarkLogo} /></Link>
+            <Link to="/"><HomeLogo src={theme === 'light' ? DarkLogo : LightLogo} /></Link>
             <div>
                 <StyledLink to="/">Accueil</StyledLink>
                 <StyledLink to="/survey/1" $isFullLink>Faire le test</StyledLink>
