5d9a53ca382211667e9b98d7320743d108f22fb2
Simon <simon@7lieues.io>
2023-05-01 16:15:14 +0200

    style: adds style to Error page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,9 +1,37 @@
+import styled from "styled-components"
+import colors from "../../utils/style/colors"
+import error from "../../assets/404.svg"
+
+const ErrorWrapper = styled.div`
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.background};
+  align-items: center;
+`
+
+const ErrorTitle = styled.h1`
+  font-weight: 300;
+`
+
+const ErrorSubtitle = styled.h2`
+  font-weight: 300;
+  color: ${colors.secondary};
+`
+
+const Illustration = styled.img`
+  max-width: 800px;
+`
+
 function Error() {
     return (
-        <div>
-            <h1>Erreur 404 !</h1>
-            <p>Cette page n'existe malheureusement pas ... </p>
-        </div>
+        <ErrorWrapper>
+            <ErrorTitle>Oups...</ErrorTitle>
+            <Illustration src={error} />
+            <ErrorSubtitle>
+                Il semblerait que la page que vous cherchez n’existe pas
+            </ErrorSubtitle>
+        </ErrorWrapper>
     )
 }
 
