16d633f5b60092a380caf9a7892abdfcbcccf464
Simon <simon@7lieues.io>
2023-05-04 23:26:00 +0200

    feat: sends Survey answers to backend

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,32 +1,117 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { Loader } from "../../utils/style/Atoms";
-import { useFetch } from "../../utils/hooks";
+import { useFetch, useTheme } from "../../utils/hooks";
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
+import { useContext } from "react";
+import { SurveyContext } from "../../utils/context";
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
+
+const ReplyBox = styled.button`
+  border: none;
+  height: 100px;
+  width: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) =>
+        theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  border-radius: 30px;
+  cursor: pointer;
+  box-shadow: ${(props) =>
+        props.isSelected ? `0px 0px 0px 2px ${colors.primary} inset` : 'none'};
+  &:first-child {
+    margin-right: 15px;
+  }
+  &:last-of-type {
+    margin-left: 15px;
+  }
+`
+
+const ReplyWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
 
 function Survey() {
     const { questionNumber } = useParams();
     const questionNumberInt = parseInt(questionNumber);
-    const previousQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
+    const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
     const nextQuestionNumber = questionNumberInt + 1;
 
+    const { theme } = useTheme();
+
+    const { answers, saveAnswers } = useContext(SurveyContext);
+    function saveReply(answer) { saveAnswers({ [questionNumber]: answer }); }
+
     const { data, isLoading, error } = useFetch('http://localhost:8000/survey');
-    const { surveyData } = data;
+    const surveyData = data?.surveyData;
 
     if (error) return <p>Oups ... Il y a eu un problème !</p>
+
     return (
-        <div>
-            <h1>Questionnaire</h1>
-            <h2>Question n°{questionNumber}</h2>
-
-            {isLoading ?
-                <Loader /> :
-                <p>{surveyData && surveyData[questionNumber]}</p>
-            }
-
-            <Link to={`/survey/${previousQuestionNumber}`}>Précédent</Link>
-            {surveyData && surveyData[nextQuestionNumber] ? <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link> : <Link to='/results'>Résultats</Link>}
-
-        </div>
+        <SurveyContainer>
+            <QuestionTitle theme={theme}>Question {questionNumber}</QuestionTitle>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <QuestionContent theme={theme}>
+                    {surveyData[questionNumber]}
+                </QuestionContent>
+            )}
+            <ReplyWrapper>
+                <ReplyBox
+                    onClick={() => saveReply(true)}
+                    isSelected={answers[questionNumber] === true}
+                    theme={theme}
+                >
+                    Oui
+                </ReplyBox>
+                <ReplyBox
+                    onClick={() => saveReply(false)}
+                    isSelected={answers[questionNumber] === false}
+                    theme={theme}
+                >
+                    Non
+                </ReplyBox>
+            </ReplyWrapper>
+            <LinkWrapper theme={theme}>
+                <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+                {surveyData && surveyData[questionNumberInt + 1] ? (
+                    <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+                ) : (
+                    <Link to="/results">Résultats</Link>
+                )}
+            </LinkWrapper>
+        </SurveyContainer>
     )
 }
 
